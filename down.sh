e4dda9720f78dc390ddb4b77747cf518625c7f74
Zerqz <Zerqz@users.noreply.github.com>
2016-11-09 19:57:04 -0700

    changed to iptables from ufw

--- down.sh
+++ down.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# Remove the ufw policys.
+# Remove the iptables policys.
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
 
+ip6tables -F
+ip6tables -t nat -F
+ip6tables -t mangle -F
+
+iptables-save
+
+echo "You can now restore your old iptable settings."
 
 exit 0
--- up.sh
+++ up.sh
@@ -1,21 +1,76 @@
 #!/bin/bash
 
-VPNSERV= wget http://ipinfo.io/ip -qO -
+#root check
+if [ "$(id -u)" != "0" ]; then
+   echo "FATAL ERROR: Run this script as root (sudo)!"
+   exit 1
+fi
 
-# Disable incoming
-ufw default deny outgoing
-ufw default deny incoming
-ufw openssh allow
+VPNSERV=$(wget http://ipinfo.io/ip -qO -)
 
-#enable outgoing
-ufw allow out on tun0 from any to any
-ufw allow in on tun0 from any to any
+#warning
+echo "Backup your existing iptables!!!!"
+read -p "Do you care? y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+    exit 1
+fi
 
-#allow from vpn server you are currently connected to
-ufw allow in from $VPNSERV to any
-ufw allow out from any to $VPNSERV
 
-#allow local traffic
-ufw allow from 192.168.1.0/24
+#flush
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+ip6tables -F
+ip6tables -t nat -F
+ip6tables -t mangle -F
+
+# Allow all and everything on localhost
+iptables -A INPUT  -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# LAN
+iptables -A INPUT  -i eth0 -s 192.168.1.0/24 -j ACCEPT
+iptables -A OUTPUT -o eth0 -d 192.168.1.0/24 -j ACCEPT
+
+# DNS
+iptables -A INPUT  -p udp --sport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+
+# Allow VPN traffic
+iptables -A INPUT  -p udp --sport 443 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 443 -j ACCEPT
+
+# VPN DNS
+iptables -A INPUT  -s 8.8.8.8 -j ACCEPT;
+iptables -A OUTPUT -s 8.8.4.4 -j ACCEPT;
+
+# VPN server
+iptables -A INPUT  -p udp -s $VPNSERV -j ACCEPT;
+iptables -A OUTPUT -p udp -d $VPNSERV -j ACCEPT;
+
+# Accept TUN
+iptables -A INPUT    -i tun+ -j ACCEPT
+iptables -A OUTPUT   -o tun+ -j ACCEPT
+iptables -A FORWARD  -i tun+ -j ACCEPT
+
+# Drop the rest
+iptables -A INPUT   -j DROP
+iptables -A OUTPUT  -j DROP
+iptables -A FORWARD -j DROP
+
+# Block All V6
+ip6tables -A OUTPUT -j DROP
+ip6tables -A INPUT -j DROP
+ip6tables -A FORWARD -j DROP
+
+#save
+iptables-save
+
+echo "..."
+echo "......"
+echo "Network locked to vpn @ $VPNSERV"
 
 exit 0
